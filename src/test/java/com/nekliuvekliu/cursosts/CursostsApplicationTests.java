83fc40159c109cd6b0de0146d588b21d0eabcba7
nelioalberto <nekliuvekliu@gmail.com>
2021-09-28 17:49:46 -0300

    projeto criado

---
+++ src/test/java/com/nekliuvekliu/cursosts/CursostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nekliuvekliu.cursosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
