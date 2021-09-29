a5c67ddbee87845e0299aa245391ebfa8ec1b61e
nelioalberto <nekliuvekliu@gmail.com>
2021-09-29 18:51:47 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/nekliuvekliu/cursosts/services/CategoriaService.java
+++ src/main/java/com/nekliuvekliu/cursosts/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.nekliuvekliu.cursosts.domain.Categoria;
 import com.nekliuvekliu.cursosts.repositories.CategoriaRepository;
+import com.nekliuvekliu.cursosts.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,7 +16,8 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id); 
-		return obj.orElse(null); 		
+		return obj.orElseThrow(() -> new ObjectNotFoundException( 
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 		
 	}
 
 }
