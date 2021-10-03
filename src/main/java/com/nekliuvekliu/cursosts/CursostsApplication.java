592ab37f4a8d654fb41b260bfd4d9bd3a709f852
nelioalberto <nekliuvekliu@gmail.com>
2021-10-03 19:18:29 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/nekliuvekliu/cursosts/CursostsApplication.java
+++ src/main/java/com/nekliuvekliu/cursosts/CursostsApplication.java
@@ -63,8 +63,13 @@ public class CursostsApplication implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria (null, "informática");
 		Categoria cat2 = new Categoria (null, "Escritório");
-	
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		Categoria cat3 = new Categoria (null, "Cama, Mesa e Banho");
+		Categoria cat4 = new Categoria (null, "Eletrônicos");
+		Categoria cat5 = new Categoria (null, "Jardinagem");
+		Categoria cat6 = new Categoria (null, "Decoração");
+		Categoria cat7 = new Categoria (null, "Perfumaria");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2, cat3, cat4, cat5, cat6, cat7));
 		
 		Produto p1 = new Produto (null, "Computador", 2000.00);
 		Produto p2 = new Produto (null, "Impressora", 800.00);
--- src/main/java/com/nekliuvekliu/cursosts/services/CategoriaService.java
+++ src/main/java/com/nekliuvekliu/cursosts/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nekliuvekliu.cursosts.domain.Categoria;
@@ -47,4 +50,10 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesForPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesForPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		}
+		
 }
