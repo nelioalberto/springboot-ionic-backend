ee974fb7ba0d6b16007c34b3e3352fd284bbc354
nelioalberto <nekliuvekliu@gmail.com>
2021-10-10 00:13:27 -0300

    Validação customizada de email não repetido na atualização

--- src/main/java/com/nekliuvekliu/cursosts/dto/ClienteDTO.java
+++ src/main/java/com/nekliuvekliu/cursosts/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.nekliuvekliu.cursosts.domain.Cliente;
+import com.nekliuvekliu.cursosts.services.validations.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/nekliuvekliu/cursosts/services/validations/ClienteUpdateValidator.java
+++ src/main/java/com/nekliuvekliu/cursosts/services/validations/ClienteUpdateValidator.java
@@ -2,44 +2,44 @@ package com.nekliuvekliu.cursosts.services.validations;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.nekliuvekliu.cursosts.domain.Cliente;
-import com.nekliuvekliu.cursosts.dto.ClienteNewDTO;
-import com.nekliuvekliu.cursosts.enums.TipoCliente;
+import com.nekliuvekliu.cursosts.dto.ClienteDTO;
 import com.nekliuvekliu.cursosts.repositories.ClienteRepository;
 import com.nekliuvekliu.cursosts.resources.exceptions.FieldMessage;
-import com.nekliuvekliu.cursosts.services.validations.utils.BR;
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private ClienteRepository repo;
 	
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
 		
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
 		List<FieldMessage> list = new ArrayList<>();
-		
-		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && ! BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
-		}
-
-		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && ! BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
-		}
 		
 		Cliente aux = repo.findByEmail(objDto.getEmail());
 		
-		if (aux != null) {
+		if (aux != null && !aux.getId().equals(uriId))  {
 			list.add(new FieldMessage("email", "E-mail já existe na base"));
 		}
 		
