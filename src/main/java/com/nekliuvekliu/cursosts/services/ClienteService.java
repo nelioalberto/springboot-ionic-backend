6b0c584b07b33bfe0f4622cab181c0bf32b255d1
nelioalberto <nekliuvekliu@gmail.com>
2021-10-09 22:58:24 -0300

    Validação customizada de CPF ou CNPJ na inserção do cliente

--- src/main/java/com/nekliuvekliu/cursosts/services/ClienteService.java
+++ src/main/java/com/nekliuvekliu/cursosts/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 		repo.deleteById(id);
 		}
 		catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir cliente com entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir cliente com pedido(s) relacionado(s)");
 		}
 	}
 	
