45ec1fbe73de4b8565ced8bf8b740291f469aaaa
nelioalberto <nekliuvekliu@gmail.com>
2021-10-14 22:55:30 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/nekliuvekliu/cursosts/config/DevConfig.java
+++ src/main/java/com/nekliuvekliu/cursosts/config/DevConfig.java
@@ -23,10 +23,10 @@ public class DevConfig {
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
 		
-		if (!"create".equals(strategy)) {
+	/*	if (!"create".equals(strategy)) {
 			return false;
 		}
-		
+		*/
 		dbService.instantiateTestDatabase();
 		return true;
 	}
